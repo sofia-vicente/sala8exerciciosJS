349708f563580eff1803089ea80e209e3d3308d7
Sofia <vicente270510@gmail.com>
2026-04-28 12:29:09 -0300

    Add files via upload

---
+++ calculos/script.js
@@ -0,0 +1,29 @@
+function somar() {
+    var n1 = document.getElementById("n1").valueAsNumber;
+    var n2 = document.getElementById("n2").valueAsNumber;
+    var resp = document.getElementById("resposta");
+    resp.textContent = n1 +n2;
+}
+    function sub() {
+    var n3 = document.getElementById("n3").valueAsNumber;
+    var n4 = document.getElementById("n4").valueAsNumber;
+    var resp = document.getElementById("resSub");
+    resp.textContent = n3 - n4;
+
+} 
+
+function mult() {
+    var n5 = document.getElementById("n5").valueAsNumber;
+    var n6 = document.getElementById("n6").valueAsNumber;
+    var resp = document.getElementById("resMult");
+    resp.textContent = n5 * n6;
+
+} 
+
+function div() {
+    var n7 = document.getElementById("n7").valueAsNumber;
+    var n8 = document.getElementById("n8").valueAsNumber;
+    var resp = document.getElementById("resDiv");
+    resp.textContent = n7 / n8;
+
+} 
