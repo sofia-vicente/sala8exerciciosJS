278c880e4bb3e210c0848f4793049958811acff9
Sofia <vicente270510@gmail.com>
2026-05-07 11:11:28 -0300

    Add files via upload

--- print/script.js
+++ print/script.js
@@ -1,21 +1,24 @@
-function mostrar (){
-    var texto = document.getElementById('i1').value
+function mostrar(){
+    var texto = document.getElementById('i1').value 
     var cor = document.getElementById('i2').value
-     var data = document.getElementById('i3').value
-     var check = document.getElementById('i4').value
+    var data = document.getElementById('i3').value
+    var check = document.getElementById('i4').checked  
+    var printDiv = document.getElementById('print')
+    var body = document.getElementById('batata')
 
 
     var printTexto = document.getElementById('printTexto')
-    var printColor = document.getElementById('printColor')
-     var printData = document.getElementById('printData')
-     var printChek = document.getElementById('printChec')
+    var printCor = document.getElementById('printCor')
+    var printData = document.getElementById('printData')
+    var printCheck = document.getElementById('printCheck')
 
-      
-
-    printTexto.innerHTML =  `o texto digitado foi :  ${texto}`
-    printColor.innerHTML = `a cor escolhida foi : ${cor}`
-    printData.innerHTML =  `a data escolhida foi :  ${data}`
-    printCheck.textContent =  `sim ou nao :  ${check}`
-
-
+    // printTexto.textContent = "O nome digitado foi : " + texto
+    printTexto.textContent = `O texto digitado foi : ${texto}`
+    printCor.innerHTML = `A cor escolhida foi : ${cor}`
+    printData.innerText = `A data escolhida foi : ${data}`
+    printCheck.textContent = `Você escolheu : ${check}`
+    printDiv.style.display = 'block'
+    printDiv.style.backgroundColor = cor
+    // body.style.backgroundColor = cor
+    // console.log(texto, typeof texto)
 }
